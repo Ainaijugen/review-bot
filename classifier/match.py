1ee0005c8211df33cf2893385aa53a5a19dc6f69
煜皓张 <zyhzyhzyh@pku.edu.cn>
2018-06-10 22:33:11 +0800

    final

--- classifier/match.py
+++ classifier/match.py
@@ -8,12 +8,34 @@ import os
 path = os.path.dirname(__file__)
 
 
-def re_matching(words):
-    for cate in tasks:
-        for kind in cate[1]:
-            if kind in words:
-                return (cate[0], kind)
-    return None
+def re_matching(words, modeltype):
+    def check(ss1, s2):
+        ans = 0
+        for s1 in ss1:
+            if s1.find(s2) != -1:
+                ans += 1
+                # print(s1, s2)
+            if s2.find(s1) != -1:
+                ans += 1
+                # print(s1, s2)
+        return ans
+
+    print(words)
+    if modeltype == -1:
+        ans = np.ones(len(tasks), dtype=np.float32)
+        for i in range(len(tasks)):
+            ans[i] += check(words, tasks[i][0])
+            for kind in tasks[i][1]:
+                ans[i] += check(words, kind)
+        ans /= np.sum(ans)
+        return ans
+    else:
+        ans = np.ones(4, dtype=np.float32)
+        ans += check(words, tasks[modeltype][0])
+        for (i, kind) in enumerate(tasks[modeltype][1]):
+            ans[i] += check(words, kind)
+        ans /= np.sum(ans)
+        return ans
 
 
 def svm_matching(words, modeltype):
@@ -33,14 +55,13 @@ def svm_matching(words, modeltype):
             pos = feature.index(words[i])
             x[0][pos] = 1
     if modeltype != -1:
-        return clf.predict_proba(x)[0]
+        return (clf.predict_proba(x)[0] + re_matching(words, modeltype) * 10) / 11
     else:
         vec = clf.predict_proba(x)[0]
         for i in range(len(tasks)):
             ans = svm_matching(words, i)
-            print(ans)
             vec[i] += np.sum(ans) / 4
-        return vec
+        return (vec / 2 + re_matching(words, modeltype) * 10) / 11
 
 
 def parse_json(filename):
@@ -53,24 +74,25 @@ def parse_json(filename):
 
 
 def query(item_name):
+    item_name = item_name.strip()
     print(item_name)
     wls = jieba.lcut(item_name)
     ans = svm_matching(wls, -1)
     tier1 = np.argmax(ans)
     tier2 = 0
-    max_proba = 0
+    max_proba = np.max(ans)
     for i in range(len(tasks)):
         ans1 = svm_matching(wls, i)
+        print(ans1)
         t_tier2 = np.argmax(ans1)
         if ans1[t_tier2] > max_proba:
             max_proba = ans1[t_tier2]
             tier2 = t_tier2
             tier1 = i
-    if max_proba >= 0.5:
-        return "%d_%d" % (tier1, tier2), max_proba
-    tier1 = np.argmax(ans)
-    if np.max(svm_matching(wls, tier1)) >= 0.5:
-        ans = svm_matching(wls, tier1)
-        tier2 = np.argmax(ans)
-        return "%d_%d" % (int(tier1), int(tier2)), np.max(ans)
-    return "%d_x" % tier1, np.max(ans) / 4
+    t_tier1 = np.argmax(ans)
+    if np.max(svm_matching(wls, t_tier1)) >= max_proba:
+        tans = svm_matching(wls, t_tier1)
+        tier2 = np.argmax(tans)
+        tier1 = t_tier1
+        max_proba = np.max(tans)
+    return "%d_%d" % (tier1, tier2), max_proba
